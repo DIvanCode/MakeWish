8494d5bd2ec9a8ab391f600c04f680b9680fd48a
divancode <divancode@gmail.com>
2025-03-28 12:47:10 +0300

    Removed unused repository

--- MakeWish.WishService/src/MakeWish.WishService.Interfaces.DataAccess/IUnitOfWork.cs
+++ MakeWish.WishService/src/MakeWish.WishService.Interfaces.DataAccess/IUnitOfWork.cs
@@ -5,7 +5,6 @@ public interface IUnitOfWork
     IWishesRepository Wishes { get; }
     IUsersRepository Users { get; }
     IWishListsRepository WishLists { get; }
-    // IWishListUserPermissionsRepository WishListUserPermissions { get; }
     
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
