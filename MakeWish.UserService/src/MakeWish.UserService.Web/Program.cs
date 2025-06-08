40091e8d1d0308afa87a61e293e199ee8303c944
Ivan Dobrynin <49978381+DIvanCode@users.noreply.github.com>
2025-06-08 19:25:38 +0300

    feat: Observability (#63)

--- MakeWish.UserService/src/MakeWish.UserService.Web/Program.cs
+++ MakeWish.UserService/src/MakeWish.UserService.Web/Program.cs
@@ -1,7 +1,10 @@
 using MakeWish.UserService.Adapters.DataAccess.EntityFramework;
 using MakeWish.UserService.Adapters.MessageBus.RabbitMQ;
+using MakeWish.UserService.Telemetry;
 using MakeWish.UserService.UseCases;
 using MakeWish.UserService.Web;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,7 @@ builder.Services.SetupWeb(builder.Configuration);
 builder.Services.SetupDataAccessEntityFramework(builder.Configuration);
 builder.Services.SetupMessageBusRabbit(builder.Configuration);
 builder.Services.SetupUseCases();
+builder.Services.SetupTelemetry(builder.Configuration);
 
 var app = builder.Build();
 
@@ -20,6 +24,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
         
 app.MapControllers();
 
